e2cb31d120e84ced5338722687d95b859a8e5a28
Stanislav Klenin <stanislav.klenin@gmail.com>
2016-07-04 23:04:33 -0300

    initial csv extractor implementation

---
+++ src/main/java/rfilter/core/CSVExtractor.java
@@ -0,0 +1,68 @@
+package rfilter.core;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
+
+public class CSVExtractor implements Extractor {
+
+    CSVReader csvr;
+
+    public static DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss zzz");
+
+    public CSVExtractor(InputStream stream) {
+        csvr = new CSVReader(new InputStreamReader(stream));
+    }
+
+    @Override
+    public Iterator<Report> iterator() {
+        return new Iter();
+    }
+
+    public class Iter implements Iterator<Report> {
+
+        String [] nextLine;
+
+        public Iter() {
+            // skip headers
+            try {
+                nextLine = csvr.readNext();
+            } catch (IOException e) {
+                e.printStackTrace();
+                nextLine = null;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            try {
+                nextLine = csvr.readNext();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return (nextLine != null);
+        }
+
+        @Override
+        public Report next() {
+            //String [] nextLine;
+            try {
+                return new Report(nextLine[0],                    // client-address
+                                  nextLine[1],                    // client-guid
+                                  DATE_FORMAT.parse(nextLine[2]), // request-time
+                                  nextLine[3],                    // service-guid
+                                  Integer.parseInt(nextLine[4]),  // retries-request
+                                  Integer.parseInt(nextLine[5]),  // packets-requested
+                                  Integer.parseInt(nextLine[6]),  // packets-serviced
+                                  Integer.parseInt(nextLine[7])); // max-hole-size
+            } catch (ParseException e) {
+                e.printStackTrace();
+                return new Report();
+            }
+        }
+    }
+}
